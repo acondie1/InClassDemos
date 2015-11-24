f9959ce50efecdf5002ada3d4fbeea53e93ff597
Ashley Condie <acondie1@studentmail.nait.ca>
2015-11-24 16:18:18 -0700

    Security configuration

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/Security/RoleManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using eRestaurantSystem.DAL.Security;
+using eRestaurantSystem.DAL.Entities.Security;
+#endregion
+
+namespace eRestaurantSystem.BLL.Security
+{
+    class RoleManager : RoleManager<IdentityRole>
+    {
+        public RoleManager() : base (new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                {
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
